f4a86170d90e58fcdbe6ec1cee183073d5b9cfc9
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2018-01-26 09:38:56 -0300

    servidor básico con express

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('hola mundo')
+'use strict'
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    success: 'Hello World'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`)
+})
