de30fdd85eed6aa4dd69e72d282729d163d20bb1
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2018-01-26 09:54:56 -0300

    configurado nodemon para funcionar con es6 y agregado formato al JSON

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+// Configura la visualización del JSON en 4 espacios.
+app.set("json spaces", 4);
+
 app.get('/', (req, res) => {
   res.json({
     success: 'Hello World'
