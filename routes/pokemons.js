44d80bf98060096c1cb989e2d00aa9ee8a813682
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2018-01-27 20:15:28 -0300

    mejorado los estilos de /pokemons

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -7,8 +7,9 @@ module.exports = app => {
       pokemon: {
         id: 6,
         name: 'Charizard',
+        description: 'Charizard Echa fuego por la boca y es capaz de derretir cualquier cosa.',
         type: 'fuego',
-        url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png'
+        url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png'
       }
     }
 
