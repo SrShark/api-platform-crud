c529c5f20a65706e47fb9fb3a030cb97024d60fe
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2018-01-29 10:20:28 -0300

    agregado /tareas y /pokedex

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 'use strict'
 
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.json({
-      success: 'Hello World'
+  app
+    .get('/', (req, res) => {
+      let locals = {
+        titulo: 'Bienvenido',
+        link: '/pokedex',
+        text: 'ir a la pokedex',
+        ir: 'keyboard_arrow_right'
+      }
+
+      res.render('index', locals)
     })
-  })
+
+    .get('/api/', (req, res) => {
+      res.json({ success: 'Hola Mundo' })
+    })
 }
