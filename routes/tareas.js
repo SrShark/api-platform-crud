a6e26686d610a4ac984c6b6b8012cbeec0fde446
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2018-02-02 21:49:32 -0300

    eliminado el id en el body del request, para que no afecte en la db

--- routes/tareas.js
+++ routes/tareas.js
@@ -3,10 +3,41 @@
 module.exports = app => {
   const Tareas = app.db.models.Tareas
 
-  app.get('/api/tareas', (req, res) => {
-    Tareas.findAll({})
-      .then(tareas => {
-        res.json({ tareas })
-      })
-  })
+  app.route('/api/tareas')
+    .all((res, req) => {
+      delete req.body.id
+      next()
+    })
+
+    .get((req, res) => {
+      Tareas.findAll({})
+        .then(tareas => {
+          res.json({ tareas })
+        })
+    })
+
+    .post((res, req) => {
+      res.json({ mensaje: 'funcionando' })
+    })
+
+  app.route('/api/tareas/:id')
+    .all((res, req) => {
+      delete req.body.id
+      next()
+    })
+
+    .get((req, res) => {
+      Tareas.findAll({})
+        .then(tareas => {
+          res.json({ tareas })
+        })
+    })
+
+    .put((res, req) => {
+      // code
+    })
+
+    .delete((res, req) => {
+      // code
+    })
 }
